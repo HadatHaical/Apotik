cc8bc639806d1e8eca665d2a68befad5ab031bdc
Haddad Yusuf <Haddad Yusuf>
2016-11-12 17:28:58 +0700

    Masih Pemula

---
+++ app/src/main/java/com/example/x550vx/aplikasiapotik/Daftar_Obat.java
@@ -0,0 +1,13 @@
+package com.example.x550vx.aplikasiapotik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Daftar_Obat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar__obat);
+    }
+}
